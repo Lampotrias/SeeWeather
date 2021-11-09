d72b4183b1364aa7fd7ce0bdd71f688ac8a289db
Mihail Mitrofanov <mitrofanov@bitrix.ru>
2021-11-09 09:51:10 +0200

    MainActivity rename to WeatherActivity

--- app/src/main/java/com/example/seeweather/WeatherActivity.kt
+++ app/src/main/java/com/example/seeweather/WeatherActivity.kt
@@ -3,7 +3,7 @@ package com.example.seeweather
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WeatherActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
