8784e70b2efd693e64af0b3ccf32ac63de49e62e
Mihail Mitrofanov <mitrofanov@bitrix.ru>
2021-11-12 10:47:41 +0200

    MainScreenFragment. Вывод информации о текущей погоде на экран

--- app/src/main/java/com/example/seeweather/presentation/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/example/seeweather/presentation/mainscreen/MainScreenFragment.kt
@@ -12,10 +12,13 @@ import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.example.seeweather.R
+import com.facebook.drawee.view.SimpleDraweeView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import java.sql.Date
 
+@Suppress("DEPRECATION")
 @AndroidEntryPoint
 class MainScreenFragment: Fragment(R.layout.main_screen_fragment) {
 
@@ -23,20 +26,32 @@ class MainScreenFragment: Fragment(R.layout.main_screen_fragment) {
 	private lateinit var cityField: EditText
 	private lateinit var submitButton: Button
 	private lateinit var stateText: TextView
+	private lateinit var icon: SimpleDraweeView
+	private lateinit var temperature: TextView
+	private lateinit var windDirection: TextView
+	private lateinit var windPower: TextView
+	private lateinit var timeOfDay: TextView
+	private lateinit var lastUpdate: TextView
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 
 		lifecycleScope.launch {
 			repeatOnLifecycle(Lifecycle.State.STARTED) {
-				viewModel.uiState.collect { state ->
-					val text = when (state) {
+				viewModel.uiState.collect { result ->
+					val text = when (result) {
 						State.INITIAL -> "INITIAL"
-						State.OK -> "OK"
-						State.ERROR -> "ERROR"
+						is State.ErrorResult -> "ERROR"
+						is State.SuccessResult -> {
+							icon.setImageURI(result.result.icon)
+							temperature.text = result.result.tempC.toString()
+							windDirection.text = result.result.windDirection.toString()
+							windPower.text = (result.result.windKph / 3.6).toString()
+							timeOfDay.text = if (result.result.isDay) "day" else "night"
+							lastUpdate.text = Date(result.result.lastUpdate * 1000).toString()
+							"SUCCESS"
+						}
 					}
-					val uri = Uri.EMPTY
-					uri.isRelative
 					stateText.text = text
 				}
 			}
@@ -48,6 +63,12 @@ class MainScreenFragment: Fragment(R.layout.main_screen_fragment) {
 
 		cityField = view.findViewById(R.id.city_field)
 		stateText = view.findViewById(R.id.state_text)
+		icon = view.findViewById(R.id.icon)
+		temperature = view.findViewById(R.id.temperature)
+		windDirection = view.findViewById(R.id.wind_direction)
+		windPower = view.findViewById(R.id.wind_power)
+		timeOfDay = view.findViewById(R.id.time_of_day)
+		lastUpdate = view.findViewById(R.id.last_update)
 		submitButton = view.findViewById<Button>(R.id.submit_btn).apply {
 			setOnClickListener { viewModel.sendRequest(cityField.text.toString()) }
 		}
--- app/src/main/java/com/example/seeweather/presentation/mainscreen/MainScreenViewModel.kt
+++ app/src/main/java/com/example/seeweather/presentation/mainscreen/MainScreenViewModel.kt
@@ -3,6 +3,7 @@ package com.example.seeweather.presentation.mainscreen
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.seeweather.domain.CurrentWeatherModel
 import com.example.seeweather.domain.RequestModel
 import com.example.seeweather.domain.WeatherRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -17,23 +18,23 @@ class MainScreenViewModel @Inject constructor(
 	private val repository: WeatherRepo
 ) : ViewModel() {
 
-	private val _uiState = MutableStateFlow(State.INITIAL)
+	private val _uiState: MutableStateFlow<State> = MutableStateFlow(State.INITIAL)
 	val uiState: StateFlow<State> = _uiState
 
 	fun sendRequest(city: String) {
 		viewModelScope.launch {
 			val result = repository.getCurrentWeather(RequestModel(city))
-			if (result.isSuccess) {
-				_uiState.value = State.OK
-			} else {
-				_uiState.value = State.ERROR
+			result.fold({
+				_uiState.value = State.SuccessResult(it)
+			}) {
+				_uiState.value = State.ErrorResult(it)
 			}
 		}
 	}
 }
 
-enum class State {
-	INITIAL,
-	OK,
-	ERROR
+sealed class State {
+	object INITIAL : State()
+	class SuccessResult(val result: CurrentWeatherModel) : State()
+	class ErrorResult(val e: Throwable) : State()
 }
