1b9fd9b40d9e8a5a34800615c9f1777ed65aeeb7
Mihail Mitrofanov <mitrofanov@bitrix.ru>
2021-11-12 10:07:03 +0200

    Gradle. Add fresco deps

--- app/src/main/java/com/example/seeweather/WeatherApp.kt
+++ app/src/main/java/com/example/seeweather/WeatherApp.kt
@@ -1,8 +1,14 @@
 package com.example.seeweather
 
 import android.app.Application
+import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class WeatherApp: Application() {
+	override fun onCreate() {
+		super.onCreate()
+
+		Fresco.initialize(this)
+	}
 }
