48418b457875b3a63fd8e09a3edd6c97ab616b17
Mihail Mitrofanov <mitrofanov@bitrix.ru>
2022-12-19 09:54:30 +0300

    восстановил UI

--- app/src/main/java/com/example/seeweather/presentation/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/example/seeweather/presentation/mainscreen/MainScreenFragment.kt
@@ -1,8 +1,51 @@
 package com.example.seeweather.presentation.mainscreen
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.seeweather.R
+import androidx.fragment.app.viewModels
+import com.example.seeweather.databinding.MainScreenFragmentBinding
+import com.example.seeweather.utils.launchAndRepeatWithViewLifecycle
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainScreenFragment : Fragment(R.layout.main_screen_fragment)
+class MainScreenFragment : Fragment() {
+	private val viewModel: MainScreenViewModel by viewModels()
+	private var _binding: MainScreenFragmentBinding? = null
+	private val binding: MainScreenFragmentBinding
+		get() = _binding!!
+
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = MainScreenFragmentBinding.inflate(layoutInflater)
+
+		binding.weatherButton.setOnClickListener {
+			viewModel.sendRequest("Kemer")
+		}
+
+		launchAndRepeatWithViewLifecycle {
+			viewModel.uiState.collect { state ->
+				if (state is State.SuccessResult) {
+					Log.e("asdasdas OK", state.result.toString())
+				} else if (state is State.ErrorResult) {
+					Log.e("asdasdas ER", state.e.toString())
+				}
+			}
+		}
+
+		return binding.root
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+
+		_binding = null
+	}
+}
+
