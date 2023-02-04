6a697f9e6b0123f5776ac963e57f7a69ca7dae52
Mihail Mitrofanov <mitrofanov@bitrix.ru>
2023-02-04 14:58:07 +0300

    build: update deps

--- app/src/main/java/com/example/seeweather/data/sources/database/dao/ServerSyncStatusDao.kt
+++ app/src/main/java/com/example/seeweather/data/sources/database/dao/ServerSyncStatusDao.kt
@@ -2,7 +2,7 @@ package com.example.seeweather.data.sources.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.seeweather.data.sources.database.model.ServerSyncTable
 
@@ -11,6 +11,6 @@ interface ServerSyncStatusDao {
 	@Query("SELECT * FROM server_status WHERE id = :serverId")
 	fun getStatus(serverId: Int): ServerSyncTable?
 
-	@Insert(onConflict = REPLACE)
+	@Insert(onConflict = OnConflictStrategy.REPLACE)
 	fun updateStatus(serverSyncTable: ServerSyncTable)
 }
